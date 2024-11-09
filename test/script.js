bae32be3527844e60cb488c9fe06a35c5b8be7b5
Peemapat Jiravatvanich-6609650566 <145252834+Xagatech@users.noreply.github.com>
2024-11-09 21:25:04 +0700

    Add files via upload

---
+++ test/script.js
@@ -0,0 +1,11 @@
+// ฟังก์ชันในการแสดง Overlay
+function showOverlay() {
+    // แสดงกล่องข้อความ Overlay
+    document.getElementById('overlay').style.display = 'flex';
+}
+
+// ฟังก์ชันในการปิด Overlay
+function closeOverlay() {
+    // ซ่อนกล่องข้อความ Overlay
+    document.getElementById('overlay').style.display = 'none';
+}
